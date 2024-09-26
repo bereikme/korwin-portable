dea95f7c5601a89cf7b1dbf5c4a3e31c28dfa32c
Khenziii <khenzii.was.taken@gmail.com>
2024-09-26 11:18:47 +0200

    feat: first commit!
    This project was bootstrapped using android studio.

---
+++ app/src/main/java/dev/khenzii/korwin_portable/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package dev.khenzii.korwin_portable.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun KorwinportableTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
